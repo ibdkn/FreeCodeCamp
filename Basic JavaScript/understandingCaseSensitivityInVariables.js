4d6ed3ce193cca71fbbb4258cb8db770b7004b31
ibdkn <ibudiakina@gmail.com>
2023-11-11 19:53:22 +0300

    added understandingCaseSensitivityInVariables.js

---
+++ Basic JavaScript/understandingCaseSensitivityInVariables.js
@@ -0,0 +1,20 @@
+// Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+//
+// Examples:
+//
+// var someVariable;
+// var anotherVariableName;
+// var thisVariableNameIsSoLong;
+// Modify the existing declarations and assignments so their names use camelCase.
+//
+// Do not create any new variables.
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
