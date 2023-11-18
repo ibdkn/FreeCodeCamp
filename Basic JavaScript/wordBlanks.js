b4b285cba27d2e9ecb12dcbfc670948df3300a50
ibdkn <ibudiakina@gmail.com>
2023-11-18 18:41:01 +0300

    added wordBlanks.js

---
+++ Basic JavaScript/wordBlanks.js
@@ -0,0 +1,15 @@
+// You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun,
+// myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not
+// change the words assigned to the variables.
+//
+// You will also need to account for spaces in your string, so that the final sentence has spaces between all the words.
+// The result should be a complete sentence.
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myNoun + " was so " + myAdjective + " that it " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
