067e3b10ffac802df73fe64fb27a1c98215d65c0
ibdkn <ibudiakina@gmail.com>
2023-11-20 22:22:15 +0300

    added shoppingList.js

---
+++ Basic JavaScript/shoppingList.js
@@ -0,0 +1,9 @@
+// There should be at least 5 sub-arrays in the list.
+
+const myList = [];
+
+myList.unshift(["Chocolate Bar", 15]);
+myList.unshift(["Chocolate Bar", 15]);
+myList.unshift(["Chocolate Bar", 15]);
+myList.unshift(["Chocolate Bar", 15]);
+myList.unshift(["Chocolate Bar", 15]);
