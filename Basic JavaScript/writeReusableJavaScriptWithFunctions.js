007eef58390a364130902faf13a7abb1136c4c89
ibdkn <ibudiakina@gmail.com>
2023-11-21 21:18:49 +0300

    added writeReusableJavaScriptWithFunctions.js

---
+++ Basic JavaScript/writeReusableJavaScriptWithFunctions.js
@@ -0,0 +1,8 @@
+// 1. Create a function called reusableFunction which prints the string Hi World to the dev console.
+// 2. Call the function.
+
+function reusableFunction() {
+    console.log("Hi World")
+}
+
+reusableFunction();
