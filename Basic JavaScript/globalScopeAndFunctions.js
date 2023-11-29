461a7a1422ceb5bc90952ed9c13458e91b7f1d64
ibdkn <ibudiakina@gmail.com>
2023-11-29 12:44:16 +0300

    added globalScopeAndFunctions.js

--- Basic JavaScript/globalScopeAndFunctions.js
+++ Basic JavaScript/globalScopeAndFunctions.js
@@ -1,6 +1,7 @@
 // Using let or const, declare a global variable named myGlobal outside of any function. Initialize it with a value of 10.
 //
 // Inside function fun1, assign 5 to oopsGlobal without using the var, let or const keywords.
+let myGlobal = 10;
 
 // Declare the myGlobal variable below this line
 function fun1() {
@@ -9,7 +10,7 @@ function fun1() {
 }
 
 // Only change code above this line
-let myGlobal = 10;
+myGlobal = 10;
 function fun2() {
     let output = "";
     if (typeof myGlobal != "undefined") {
