9a49322a0da701e5632706566e00c0c5b6325f21
ibdkn <ibudiakina@gmail.com>
2023-11-30 12:24:19 +0300

    completed comparisonsWithTheLogicalAndOperator.js

--- Basic JavaScript/comparisonsWithTheLogicalAndOperator.js
+++ Basic JavaScript/comparisonsWithTheLogicalAndOperator.js
@@ -4,10 +4,8 @@
 function testLogicalAnd(val) {
     // Only change code below this line
 
-    if (val) {
-        if (val) {
-            return "Yes";
-        }
+    if (val <= 50 && val >= 25) {
+        return "Yes";
     }
 
     // Only change code above this line
