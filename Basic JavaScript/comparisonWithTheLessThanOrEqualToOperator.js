3a754bcebc673778f914fe13156cbbbf2ed71add
ibdkn <ibudiakina@gmail.com>
2023-11-30 12:21:23 +0300

    completed comparisonWithTheLessThanOrEqualToOperator.js

--- Basic JavaScript/comparisonWithTheLessThanOrEqualToOperator.js
+++ Basic JavaScript/comparisonWithTheLessThanOrEqualToOperator.js
@@ -1,11 +1,11 @@
 // Add the less than or equal to operator to the indicated lines so that the return statements make sense.
 
 function testLessOrEqual(val) {
-    if (val) {  // Change this line
+    if (val <= 12) {  // Change this line
         return "Smaller Than or Equal to 12";
     }
 
-    if (val) {  // Change this line
+    if (val <= 24) {  // Change this line
         return "Smaller Than or Equal to 24";
     }
 
