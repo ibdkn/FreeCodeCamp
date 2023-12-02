f74ae5c52c15817ea63f27e8daad576be4393549
ibdkn <ibudiakina@gmail.com>
2023-12-02 22:56:43 +0300

    completed nestingForLoops.js

--- Basic JavaScript/nestingForLoops.js
+++ Basic JavaScript/nestingForLoops.js
@@ -1,12 +1,16 @@
 // Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
 
-let product = 1;
-// Only change code below this line
-for (let i = 0; i < arr.length; i++) {
-    const subArray = arr[i];
-    for (let j = 0; j < subArray.length; j++) {
-        product *= subArray[j];
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        const subArray = arr[i];
+        for (let j = 0; j < subArray.length; j++) {
+            product *= subArray[j];
+        }
     }
+    // Only change code above this line
+    return product;
 }
-// Only change code above this line
-return product;
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
