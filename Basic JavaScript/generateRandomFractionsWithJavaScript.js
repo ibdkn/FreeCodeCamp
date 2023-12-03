91b6d1281e29036a4c8ac342c7bcccd6ac01400a
ibdkn <ibudiakina@gmail.com>
2023-12-03 21:14:31 +0300

    completed generateRandomFractionsWithJavaScript.js

--- Basic JavaScript/generateRandomFractionsWithJavaScript.js
+++ Basic JavaScript/generateRandomFractionsWithJavaScript.js
@@ -1,1 +1,10 @@
 // Change randomFraction to return a random number instead of returning 0.
+
+function randomFraction(num) {
+
+    // Only change code below this line
+
+    return Math.random(num);
+
+    // Only change code above this line
+}
