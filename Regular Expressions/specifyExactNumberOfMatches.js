121fa3214e0f81b661c41003cb39fd5e71d5c89d
ibdkn <ibudiakina@gmail.com>
2023-12-21 12:59:38 +0300

    completed specifyExactNumberOfMatches.js

---
+++ Regular Expressions/specifyExactNumberOfMatches.js
@@ -0,0 +1,5 @@
+// Change the regex timRegex to match the word Timber only when it has four letter m's.
+
+let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/; // Change this line
+let result = timRegex.test(timStr);
