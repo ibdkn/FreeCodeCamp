cba1f30d869c8b1d0a6449e2521e4b545e0dcbf8
ibdkn <ibudiakina@gmail.com>
2023-12-22 12:47:51 +0300

    completed useTypeofToCheckTheTypeOfAVariable.js

---
+++ Debugging/useTypeofToCheckTheTypeOfAVariable.js
@@ -0,0 +1,7 @@
+// Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
+
+let seven = 7;
+let three = "3";
+console.log(typeof seven);
+console.log(typeof three);
+// Only change code below this line
