26cdccd85da3d90677a1178c2b5904253f13ccda
ibdkn <ibudiakina@gmail.com>
2024-01-04 22:54:42 +0300

    completed sliceAndSplice.js

--- Basic Algorithm Scripting/sliceAndSplice.js
+++ Basic Algorithm Scripting/sliceAndSplice.js
@@ -6,3 +6,14 @@
 //
 // Return the resulting array. The input arrays should remain the same after the function runs.
 
+function frankenSplice(arr1, arr2, n) {
+    // It's alive. It's alive!
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+        localArray.splice(n, 0, arr1[i]);
+        n++;
+    }
+    return localArray;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
