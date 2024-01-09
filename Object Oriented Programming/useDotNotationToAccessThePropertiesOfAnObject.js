4b94099d5c81aceb10ddcbc4a4b07ffb67cf072c
ibdkn <ibudiakina@gmail.com>
2024-01-09 21:53:41 +0300

    completed useDotNotationToAccessThePropertiesOfAnObject.js

--- Object Oriented Programming/useDotNotationToAccessThePropertiesOfAnObject.js
+++ Object Oriented Programming/useDotNotationToAccessThePropertiesOfAnObject.js
@@ -1,2 +1,9 @@
 // Print both properties of the dog object to your console.
 
+let dog = {
+    name: "Spot",
+    numLegs: 4
+};
+// Add your code below this line
+console.log(dog.name);
+console.log(dog.numLegs);
