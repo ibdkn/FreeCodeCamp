c322c90ca4cab29a9b7df0571a0af3ae54b24621
ibdkn <ibudiakina@gmail.com>
2024-01-10 23:04:16 +0300

    completed understandThePrototypeChain.js

---
+++ Object Oriented Programming/understandThePrototypeChain.js
@@ -0,0 +1,12 @@
+// The hasOwnProperty method is defined in Object.prototype, which can be accessed by Bird.prototype, which can then be accessed by duck. This is an example of the prototype chain. In this prototype chain, Bird is the supertype for duck, while duck is the subtype. Object is a supertype for both Bird and duck. Object is a supertype for all objects in JavaScript. Therefore, any object can use the hasOwnProperty method.
+
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
